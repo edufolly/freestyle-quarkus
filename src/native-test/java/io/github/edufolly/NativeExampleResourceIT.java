29f62a9fd831c76cfa38aaf12a3bd2901bc88b46
Eduardo Folly <edufolly@gmail.com>
2021-02-08 18:28:39 -0300

    First commit.

---
+++ src/native-test/java/io/github/edufolly/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package io.github.edufolly;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
